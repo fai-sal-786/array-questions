5628cd5be486fe21cfb5f5c681125116796ecbad
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-05-20 04:16:08 -0700

    Array question

---
+++ array questions/app14.js
@@ -0,0 +1,87 @@
+// Q1: Declare an empty array using js literal notation to store student names in future?
+
+// let futureStudentNames = [];
+// console.log(futureStudentNames);
+
+// Q2:Declare an empty array using js object notation to store student names in future?
+
+// let futureStudentNames = [];
+
+// futureStudentNames['student1'] = 'JOHN';
+// futureStudentNames['student2'] = 'Alice';
+// console.log(futureStudentNames.student1);
+// console.log(futureStudentNames.student2);
+
+// Q3:Declare and initialize a string array?
+
+// let stringArray = ['APPLE','Banana','orange'];
+// // console.log(stringArray);
+
+// Q4:Declare and initialize a numbers array?
+
+// let numbersArray = [1,2,3,4,5];
+// console.log(numbersArray);
+
+// Q5: Declare and initialize a boolen array?
+
+// let booleanArray = [true, false, true, false];
+// console.log(booleanArray);
+
+// Q6: Declare and initialize a mixed array?
+
+// let mixedArray = [1, 'apple', true, { name: 'John', age: 25 }, null];
+// console.log(mixedArray);
+
+// Q7:Declare and Initialize an array and store available mobile networks in Pakistan?
+
+// let mobileNetworksInPakistan = ['Mobilink', 'Telenor', 'Zong', 'Ufone', 'Warid'];
+// console.log(mobileNetworksInPakistan);
+
+// Q8:Declare and Initialize an array and store available education qualifications in pakistan(SSC, HSC, BCS, BS, BCOM, MS, MPHIL, PHD). Show the listed qualification in your browser like?
+
+// let qualifications = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'MPHIL', 'PHD'];
+// document.write("<ul>");
+
+// qualifications.forEach(function(qualification) {
+    // document.write("<li>" + qualification + "<li>");
+
+// });
+// document.write("<ul");
+
+// Q9:Declare and initialize an empty array to store top movies of 2015. Add movies one by one in an array. Display the elements & length of array in your browser(use arrays length method)
+// 1)Avengers: Age of ultron
+// 2)Spectre
+// 3) Jurrassic world
+// 4) Inside out?
+
+// let topMovies2015 = [];
+
+// topMovies2015.push("Avengers: Age of ultron");
+// topMovies2015.push("Spectre");
+// topMovies2015.push("Jurrassic World");
+// topMovies2015.push("Inside out");
+
+// document.write("<p>Top Movies Of 2015:</p>");
+// document.write("<ul>");
+// topMovies2015.forEach(function(movie) {
+    // document.write("<li>" + movie + "<li>");
+// });
+// document.write("<ul>");
+
+// document.write("<p>Number Of Movies:" + topMovies2015.length + "</p>");
+
+// Q10: Declare and Initialize an array with your favourite cars Show. a) First index of the array. b)Car at first index of the array: Audi. c) Last index of the array d) Cars at last index of the array d) car at last index of the array ?
+
+// let favouriteCars = ["Audi", "volvo", "Ford", "Lamborgini"];
+// let firstIndex = 0;
+// let carAtFirstIndex = favouriteCars[firstIndex];
+// let lastIndex = favouriteCars.length - 1;
+// let carsAtLastIndex = favouriteCars[lastIndex];
+// let carAtLastIndex = favouriteCars[favouriteCars.length - 1];
+
+// console.log("a) First index of the array:", firstIndex);
+// console.log("b) Car at first index of the array:", carAtFirstIndex);
+// console.log("c) Last index of the array : ", lastIndex);
+// console.log("d) cars At last index of the array:", carsAtLastIndex);
+// console.log("e) car At last index of the array:", carAtLastIndex);
+
